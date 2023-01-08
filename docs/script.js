d09eb04b7291759770b2358b81d8cdd1e3e19e4f
Ourou7 <nathansutcliffeccc@gmail.com>
2023-01-08 18:36:59 +0000

    changed file structure

--- docs/script.js
+++ docs/script.js
@@ -91,6 +91,5 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
